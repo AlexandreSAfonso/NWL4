e55614a85b182c6fd1eb913b9b8ffd547f0d4430
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-02-24 18:25:23 -0300

    Initial commit

---
+++ api/src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express'; 
+
+const app = express();
+
+/**
+ * http://localhost:3333/
+ * / 1 Param = Rota
+ * / 2 Param Request, response
+ * GET == Busca
+ * POST == Salvar
+ * PUT == Alterar
+ * DELETE == Deletar
+ * PATCH == Alteração Específica
+ */
+app.get("/", (request, response) =>{
+    return response.send("Use default requests");
+});
+app.post("/", (request, response) =>{
+    return response.json({message: "Dados salvos com sucesso"});
+});
+ app.get("/users", (request, response) =>{
+    
+    //return response.send("Hello World - NLW04"); formato simples abaixo, formato json
+    return response.json({
+        message: "Hello World - NLW04",
+        request: "Users"
+    });
+});
+
+
+app.listen(3333, () => console.log("Server is Running!"))
