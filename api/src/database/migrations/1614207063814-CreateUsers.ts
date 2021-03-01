9d464e1bb91e1bef7e27069c95982440a59b5f97
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-03-01 12:14:20 -0300

    Create Users Migrate

--- api/src/database/migrations/1614207063814-CreateUsers.ts
+++ api/src/database/migrations/1614207063814-CreateUsers.ts
@@ -1,11 +1,40 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {Column, MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1614207063814 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns:[
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                ],
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
     }
 
 }
+/**
+ * para rodar as migrations, use no console: "yarn typeorm migration:run"
+*/
